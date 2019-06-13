668284a1916a6296bb7c1fd4a83cc565b7e24287
Marla Defensor <47424316+marladefensor@users.noreply.github.com>
2019-06-13 11:17:07 -0400

    added modal functionality

--- js/app.js
+++ js/app.js
@@ -2,15 +2,13 @@
  * Create a list that holds all of your cards
  */
 let timer = false;
-const cards = ['fa-diamond','fa-diamond','fa-paper-plane-o','fa-paper-plane-o','fa-anchor','fa-anchor',
-              'fa-bolt','fa-bolt','fa-cube','fa-cube','fa-leaf','fa-leaf','fa-bicycle','fa-bicycle',
-              'fa-bomb','fa-bomb'];
-// console.log(cards.length);
-// console.log(cards);
+let time = 0;
+const cards = ['far fa-gem','far fa-gem','far fa-paper-plane','far fa-paper-plane','fas fa-anchor','fas fa-anchor',
+              'fas fa-bolt','fas fa-bolt','fas fa-cube','fas fa-cube','fas fa-leaf','fas fa-leaf','fas fa-bicycle','fas fa-bicycle',
+              'fas fa-bomb','fas fa-bomb'];
 
-// console.log(shuffle(cards));
 display(cards);
-// console.log(document.getElementById('deck'));
+console.log(cards);
 
 /*
  * Display the cards on the page
@@ -25,7 +23,7 @@ function display(array) {
     let list = document.getElementById('deck');
     let newListItem = document.createElement('li');
     newListItem.classList.add('card');
-    newListItem.innerHTML = `<i class="fa ${item}"></i>`;
+    newListItem.innerHTML = `<i class="${item}"></i>`;
     list.appendChild(newListItem);
   }
 }
@@ -61,10 +59,7 @@ let matched = []; // array to hold all the matched items
 
 document.addEventListener('click', (event) => {
   event.preventDefault();
-  if (timer == false) {
-    setTimer();
-  }
-
+  setTimer();
   if (event.target.classList.value == 'card') {
     let card = event.target;
     console.log(card.classList.value);
@@ -74,6 +69,7 @@ document.addEventListener('click', (event) => {
   }
 })
 
+// reloads the page if the restart icon is pressed
 let restart = document.getElementById('restart');
 
 restart.addEventListener('click', (event) => {
@@ -81,6 +77,7 @@ restart.addEventListener('click', (event) => {
   document.location.reload(true);
 })
 
+// checks if the two cards selected matches, if all cards are matched, win function is called
 function matchList(card) {
   matches.push(card);
   if (matches.length == 2) {
@@ -91,6 +88,7 @@ function matchList(card) {
   }
 }
 
+// checks if the two cards selected matched, if not, cards are turned over
 function check(arr1,arr2) {
   if (arr1.innerHTML == arr2.innerHTML) {
     arr1.classList.add('match');
@@ -114,17 +112,59 @@ function check(arr1,arr2) {
   matches = [];
 }
 
+// starts the timer up
 function setTimer() {
-  
+  if (timer == false) {
+    timer = true;
+    let start = Date.now();
+    setInterval(function() {
+      time = Date.now() - start;
+    },1000);
+  }
 }
 
+// converts final time from seconds to a timer format
+// time conversion found on: https://www.codespeedy.com/convert-seconds-to-hh-mm-ss-format-in-javascript/
+function finalTime(totalSecs) {
+  let hours = Math.floor(totalSecs/3600);
+  totalSecs %= 3600;
+  let min = Math.floor(totalSecs/60);
+  let secs = totalSecs % 60;
+  return `${hours}:${min}:${secs}`;
+}
+
+// increments the moves number when a card is selected, changes star rating
 function moveUp() {
   let moves = document.getElementById('moves');
   // console.log('moves: ', moves.innerHTML);
   moves.innerHTML = parseFloat(moves.innerHTML) + 1;
   // console.log('after moves: ', moves.innerHTML);
+  if (parseFloat(moves.innerHTML) > 40) {
+    document.getElementById('third').innerHTML = '<i class="far fa-star"></i>';
+  }
+  else if(parseFloat(moves.innerHTML) > 60) {
+    document.getElementById('second').innerHTML = '<i class="far fa-star"></i>';
+  }
+  else if(parseFloat(moves.innerHTML) > 80) {
+    document.getElementById('first').innerHTML = '<i class="far fa-star"></i>';
+  }
 }
 
+// makes modal visible when the player wins the game
 function win(){
-  alert(`You win!!\nYour score is: ${document.getElementById('moves').innerHTML}`);
+  let final = finalTime(Math.floor(time/1000));
+  let modal = document.getElementById('modalWin');
+  document.getElementById('modal-content').innerHTML =
+    `<p>You win!</p>
+    <p> Your star rating is: PUT HERE</p>
+    <p> Score: ${document.getElementById('moves').innerHTML}</p>
+    <p> Time: ${final}</p>
+    <p id="tryAgain">Try Again?</p>`;
+
+  document.getElementById('tryAgain').addEventListener('click',  (event) => {
+    event.preventDefault();
+    document.location.reload(true);});
+
+  modal.style.display = "block";
+  // setTimeout(alert(`You win!!\nYour score is: ${document.getElementById('moves').innerHTML}`),4000);
 }
